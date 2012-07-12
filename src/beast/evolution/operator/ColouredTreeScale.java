dbde7693af3d0aa19c4151883663d9e559e950ab
Tim Vaughan <tgvaughan@gmail.com>
2012-07-12 17:52:51 +1200

    Completed draft of tree scaling operator.

--- src/beast/evolution/operator/ColouredTreeScale.java
+++ src/beast/evolution/operator/ColouredTreeScale.java
@@ -37,22 +37,58 @@ public class ColouredTreeScale extends ColouredTreeOperator {
 	@Override
 	public double proposal() {
 
-		double logHR = 0;
+		// Record old tree height:
+		double h = tree.getRoot().getHeight();
 
 		// Choose scale factor:
 		double u = Randomizer.nextDouble();
 		double f = u*m_scaleParam.get() + (1.0-u)/m_scaleParam.get();
 
 		// Scale internal node heights:
-
 		for (Node node : tree.getInternalNodes())
-			node.setHeight(node.getHeight()*u);
+			node.setHeight(node.getHeight()*f);
 
-		if (u<1.0) {
-			// Check ranking of nodes hasn't been broken.
+		// Ensure new node heights make sense, force reject if not:
+		if (u<1.0 && !validateSubtreeNodeHeights(tree.getRoot()))
+				return Double.NEGATIVE_INFINITY;
+
+		// Scale colour change times:
+		for (Node node : tree.getNodesAsArray()) {
+			for (int c=0; c<cTree.getChangeCount(node); c++) {
+				double oldTime = cTree.getChangeTime(node, c);
+				setChangeTime(node, c, f*oldTime);
+			}
 		}
 
-		return logHR;
+		// New tree height:
+		double hPrime = tree.getRoot().getHeight();
+
+		// Return Hastings ratio:
+		return Math.log(h) - Math.log(hPrime);
+	}
+
+	/**
+	 * Check whether the heights of all nodes in the subtree of node
+	 * are smaller than those of their parents.
+	 * 
+	 * @param node
+	 * @return True if heights satisfy the condition.
+	 */
+	public boolean validateSubtreeNodeHeights(Node node) {
+
+		if (!node.isRoot()) {
+			if (node.getHeight()>node.getParent().getHeight())
+				return false;
+		}
+
+		if (!node.isLeaf()) {
+			for (Node child : node.getChildren()) {
+				if (!validateSubtreeNodeHeights(child))
+					return false;
+			}
+		}
+
+		return true;
 	}
 	
 }
