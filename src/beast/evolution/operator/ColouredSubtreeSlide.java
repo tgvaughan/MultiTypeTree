ba5d0e31d860d202aca6f1a8507b24acc0149842
Tim Vaughan <tgvaughan@gmail.com>
2012-07-28 16:59:29 +1200

    Root STS move seems to be working!

--- src/beast/evolution/operator/ColouredSubtreeSlide.java
+++ src/beast/evolution/operator/ColouredSubtreeSlide.java
@@ -78,69 +78,32 @@ public class ColouredSubtreeSlide extends ColouredTreeOperator {
 			double f = u*rootSlideParam + (1.0-u)/rootSlideParam;
 			double tMin = Math.max(t_node, t_sister);
 			double t_parentNew = tMin + f*(t_parentOld-tMin);
-			
-			if (t_parentNew>t_parentOld) {
-				// Root age INCREASE move
 
-				// Record old number of colour change events:
-				int nChangesOld = cTree.getChangeCount(node);
+			// Record old number of colour change events:
+			int nChangesNodeOld = cTree.getChangeCount(node);
+			int nChangesSisterOld = cTree.getChangeCount(sister);
 
-				// Select number of colour change events:
-				double L = (t_parentNew - t_node) + (t_parentNew - t_parentOld);
-				int nChanges = PoissonRandomizer.nextInt(mu*L);
+			// Select number of colour change events:
+			int nChangesNode = PoissonRandomizer.nextInt(mu*(t_parentNew-t_node));
+			int nChangesSister = PoissonRandomizer.nextInt(mu*(t_parentNew-t_sister));
 
-				// Record forward move probability:
-				logHR -= getLogMoveProb(node, t_parentNew, nChanges);
-				
-				// Assign new height to parent:
-				parent.setHeight(t_parentNew);
-				
-				// Recolour branch between node, up to root, then down toward
-				// sister to a height of tOld.  Reject outright if final colour
-				// at the end of this path on sister is different to the original
-				// colour at that point.
-				if (!recolourRootBranch(node, t_parentOld, nChanges))
-					return Double.NEGATIVE_INFINITY;
-				
-				// Record reverse move probability:
-				logHR += getLogMoveProb(node, t_parentOld, nChangesOld);
+			// Record forward move probability:
+			logHR -= getLogRootMoveProb(node, t_parentNew,
+					nChangesNode, nChangesSister);
 
-			} else {
-				// Root age DECREASE move:
-				
-				// Record old number of colour change events:
-				int nChangesOld = cTree.getChangeCount(node);
-				int i = cTree.getChangeCount(sister)-1;
-				while (i>=0 && cTree.getChangeTime(sister, i)>t_parentOld) {
-					nChangesOld += 1;
-					i -= 1;
-				}
+			// Assign new height to parent:
+			parent.setHeight(t_parentNew);
 
-				// Select number of colour change events:
-				double L = t_parentNew - t_node;
-				int nChanges = PoissonRandomizer.nextInt(mu*L);
-				
-				// Record forward move probability:
-				logHR -= getLogMoveProb(node, t_parentNew, nChanges);
+			// Recolour branch between node, up to root, then down toward
+			// sister to a height of tOld.  Reject outright if final colour
+			// at the end of this path on sister is different to the original
+			// colour at that point.
+			if (!recolourRoot(node, nChangesNode, nChangesSister))
+				return Double.NEGATIVE_INFINITY;
 
-				// Assign new height to parent:
-				parent.setHeight(t_parentNew);
-
-				// Update colour changes between sister and root to reflect
-				// change:
-				removeExcessColours(getOtherChild(parent, node));
-
-				// Recolour branch between node and root.  Reject outright if
-				// final colour is different to existing colour on root node.
-				if (!recolourBranch(node, nChanges)) {
-					// DEBUG:
-					//System.out.println("Reject.");
-					return Double.NEGATIVE_INFINITY;
-				}
-								
-				// Record reverse move probability:
-				logHR += getLogMoveProb(node, t_parentOld, nChangesOld);
-			}
+			// Record reverse move probability:
+			logHR += getLogRootMoveProb(node, t_parentOld,
+					nChangesNodeOld, nChangesSisterOld);
 
 		} else {
 
@@ -162,7 +125,8 @@ public class ColouredSubtreeSlide extends ColouredTreeOperator {
 	 * @param nChanges
 	 * @return Log of move probability.
 	 */
-	double getLogMoveProb(Node node, double newParentTime, int nChanges) {
+	double getLogRootMoveProb(Node node, double newParentTime,
+			int nChangesNode, int nChangesSister) {
 		double logP = 0;
 		
 		Node parent = node.getParent();
@@ -172,31 +136,34 @@ public class ColouredSubtreeSlide extends ColouredTreeOperator {
 		double t_parentOld = parent.getHeight();
 		double t_sister = sister.getHeight();
 		
-		double tMinHard = Math.max(node.getHeight(), sister.getHeight());
-		double tMin = (t_parentOld - tMinHard)/rootSlideParam + tMinHard;
-		double tMax = (t_parentOld - tMinHard)*rootSlideParam + tMinHard;
+		double tMinHard = Math.max(t_node,t_sister);
+		double Told = t_parentOld - tMinHard;
+		double Tnew = newParentTime - tMinHard;
 		
 		// Reject outright if proposed time outside of bounds:
-		if (newParentTime>tMax || newParentTime<tMin)
+		if (Tnew/Told<1.0/rootSlideParam || Tnew/Told>rootSlideParam)
 			return Double.NEGATIVE_INFINITY;
 		
 		// Probability of selecting new time:
-		logP += Math.log(1.0/(t_parentOld - tMinHard));
+		logP += Math.log(1.0/Told);
 		
 		// Probability of selecting nChanges colour change events:
-		double L;
-		if (node.getParent().isRoot() && newParentTime > t_parentOld)
-			L = (newParentTime-t_node) + (newParentTime-t_parentOld);
-		else
-			L = newParentTime-t_node;
-		logP += -mu*L + nChanges*Math.log(mu*L);
-				//- GammaFunction.lnGamma(nChanges+1);
+		double Lnode = newParentTime - t_node;
+		double Lsister = newParentTime - t_sister;
+		logP += -Lnode*mu + nChangesNode*Math.log(Lnode*mu)
+				- GammaFunction.lnGamma(nChangesNode+1);
+		logP += -Lsister*mu + nChangesSister*Math.log(Lsister*mu)
+				- GammaFunction.lnGamma(nChangesSister+1);
+
+		
+		// Probability of selecting particular colour change times:
+		logP += GammaFunction.lnGamma(nChangesNode+1)
+				- nChangesNode*Math.log(Lnode);
+		logP += GammaFunction.lnGamma(nChangesSister+1)
+				- nChangesSister*Math.log(Lsister);
 		
 		// Probability of selecting a particular colour assignment:
-		logP += nChanges*Math.log(1.0/(cTree.getNColours()-1));
-		
-		// Probability of drawing a particular sequence of ordered change times:
-		logP += -(nChanges*Math.log(L)); // - GammaFunction.lnGamma(nChanges+1));
+		logP += (nChangesNode+nChangesSister)*Math.log(1.0/(cTree.getNColours()-1));
 				
 		return logP;
 	}
@@ -208,54 +175,8 @@ public class ColouredSubtreeSlide extends ColouredTreeOperator {
 	 * @param nChanges 
 	 * @return True if selected colour change consistent.
 	 */
-	private boolean recolourBranch(Node node, int nChanges) {
-
-		// Early exit for single-coloured trees:
-		if (cTree.getNColours()<2)
-			return true;
-		
-		Node parent = node.getParent();
-		double t_node = node.getHeight();
-		double t_parent = parent.getHeight();
-		int nodeCol = cTree.getNodeColour(node);
-		int parentCol = cTree.getNodeColour(parent);
-		
-		// Clear current changes:
-		setChangeCount(node, 0);
-		
-		// Early exit if no changes scheduled:
-		if (nChanges==0) {
-			if (nodeCol != parentCol)
-				return false;
-			else
-				return true;
-		}
-
-		// Obtain colours and times of change events:
-		double L = t_parent - t_node;
-		double[] times = getTimes(L, nChanges);
-		int[] colours = getColours(cTree.getNodeColour(node), nChanges);
-		
-		// Force reject if final colour doesn't match parent colour:
-		if (colours[nChanges-1] != parentCol)
-			return false;
-		
-		// Add newly selected colour changes to branch:
-		for (int i=0; i<times.length; i++)
-			addChange(node, colours[i], times[i] + t_node);
-
-		return true;
-	}
-
-	/**
-	 * Recolour branch from node to root then back down toward sister to age
-	 * tOnSister with random colour changes.
-	 * 
-	 * @param node
-	 * @param tOnSister 
-	 */
-	private boolean recolourRootBranch(Node node, double tOnSister,
-			int nChanges) {
+	private boolean recolourRoot(Node node,
+			int nChangesNode, int nChangesSister) {
 
 		// Early exit for single-coloured trees:
 		if (cTree.getNColours()<2)
@@ -263,83 +184,52 @@ public class ColouredSubtreeSlide extends ColouredTreeOperator {
 		
 		Node parent = node.getParent();
 		Node sister = getOtherChild(parent, node);
+		
 		double t_node = node.getHeight();
 		double t_parent = parent.getHeight();
+		double t_sister = sister.getHeight();
+		
 		int nodeCol = cTree.getNodeColour(node);
-		int finalCol = cTree.getColourOnBranch(sister, tOnSister);
-
-		// Clear existing changes above node
-		// (shouldn't be any changes above tOnSister on sister)
+		int sisterCol = cTree.getNodeColour(sister);
+		
+		// Clear current changes:
 		setChangeCount(node, 0);
-				
-		// Early exit if no changes scheduled:
-		if (nChanges == 0) {
-			if (nodeCol != finalCol)
-				return false;
-			else
-				return true;
-		}
-
-		// Obtain colours and times of change events:
-		double L = 2*t_parent - t_node - tOnSister;
-		double[] times = getTimes(L, nChanges);
-		int[] colours = getColours(nodeCol, nChanges);
+		setChangeCount(sister, 0);
 		
-		// Force reject if chosen final colour doesn't match
-		// required final colour:
-		if (colours[nChanges-1] != finalCol)
-			return false;
-		
-		// Add colours to branch above node:
-		int lastCol = nodeCol;
-		int i=0;
-		while (i<nChanges && times[i] < t_parent-t_node) {
-			addChange(node, colours[i], times[i]+t_node);
-			lastCol = colours[i];
-			i += 1;
-		}
-		
-		// Set root node colour on the way past:
-		setNodeColour(parent, lastCol);
-		
-		// Add colours to branch above sister node:
-		for (int j=nChanges-1; j>=i; j--) {
-			double thisTime = (L - times[j]) + tOnSister;
-			int thisCol;
-			if (j>0)
-				thisCol = colours[j-1];
-			else
-				thisCol = lastCol;
+		// Assign changes on branch above node:
+		if (nChangesNode != 0) {
+			double[] times = getTimes(t_parent-t_node, nChangesNode);
+			int[] colours = getColours(nodeCol, nChangesNode);
+			for (int i=0; i<nChangesNode; i++) {
+				addChange(node, colours[i], times[i] + t_node);
+			}
+			setNodeColour(parent, colours[nChangesNode-1]);
 			
-			addChange(sister, thisCol, thisTime);
+		} else {
+			setNodeColour(parent, nodeCol);
 		}
 		
-		return true;
-	}
+		int parentCol = cTree.getNodeColour(parent);
+		
+		// Assign changes on branch above sister:
+		if (nChangesSister != 0) {
+			double [] times = getTimes(t_parent-t_sister, nChangesSister);
+			int [] colours = getColours(sisterCol, nChangesSister);
 
-	/**
-	 * Remove colour changes on branch between node and its parent which
-	 * occur at times greater than the age of the parent.
-	 * 
-	 * @param node 
-	 */
-	private void removeExcessColours(Node node) {
-
-		double tParent = node.getParent().getHeight();
-
-		int newChangeCount = 0;
-		while (newChangeCount<cTree.getChangeCount(node) &&
-			cTree.getChangeTime(node, newChangeCount)<tParent) {
-			newChangeCount++;
+			// Force reject if colouring inconsistent:
+			if (colours[nChangesSister-1]!=parentCol)
+				return false;
+			
+			for (int i=0; i<nChangesSister; i++)
+				addChange(sister, colours[i], times[i] + t_sister);
+			
+		} else {
+			// Force reject if colouring inconsistent:
+			if (parentCol != sisterCol)
+				return false;
 		}
 
-		setChangeCount(node, newChangeCount);
-
-		// Ensure colour at parent node consistent with last change:
-		setNodeColour(node.getParent(), cTree.getFinalBranchColour(node));
-
-		// DEBUG:
-		// System.out.println("After REC: " + cTree.getFlattenedTree());
+		return true;
 	}
 	
 	/**
