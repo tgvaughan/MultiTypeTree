39da91a6a610ce0565e2719452b204352d2f7b46
Tim Vaughan <tgvaughan@gmail.com>
2012-12-03 16:54:03 +1300

    Added scalar parameter scaling to coloured tree scale operator.

--- src/beast/evolution/operators/ColouredTreeScale.java
+++ src/beast/evolution/operators/ColouredTreeScale.java
@@ -18,17 +18,31 @@ package beast.evolution.operators;
 
 import beast.core.Description;
 import beast.core.Input;
+import beast.core.parameter.RealParameter;
 import beast.evolution.tree.Node;
 import beast.util.Randomizer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- *
  * @author Tim Vaughan <tgvaughan@gmail.com>
  */
-@Description("Scale operator for coloured trees.")
+@Description("Scale operator for coloured trees.  Also allows additional "
+        + "scalar parameters to be rescaled (either forward or inversely) "
+        + "at the same time.")
 public class ColouredTreeScale extends ColouredTreeOperator {
+    
+    public Input<List<RealParameter>> scalarParamsInput =
+            new Input<List<RealParameter>>("scalarParam",
+            "Scale this scalar parameter by the same amount as tree.",
+            new ArrayList<RealParameter>());
+    
+    public Input<List<RealParameter>> scalarParamsInverseInput =
+            new Input<List<RealParameter>>("scalarParamInverse",
+            "Scale this scalar parameter inversely.",
+            new ArrayList<RealParameter>());
 
-    public Input<Double> m_scaleParam = new Input<Double>("scaleParam",
+    public Input<Double> scaleFactorInput = new Input<Double>("scaleFactor",
             "Scaling is restricted to the range [1/scaleFactor, scaleFactor]");
 
     @Override
@@ -38,16 +52,12 @@ public class ColouredTreeScale extends ColouredTreeOperator {
     @Override
     public double proposal() {
 
-        // Is there a better way to do this?
         cTree = colouredTreeInput.get();
         tree = cTree.getUncolouredTree();
 
-        // Record old tree height:
-        double hOld = tree.getRoot().getHeight();
-
         // Choose scale factor:
         double u = Randomizer.nextDouble();
-        double f = u*m_scaleParam.get()+(1.0-u)/m_scaleParam.get();
+        double f = u*scaleFactorInput.get()+(1.0-u)/scaleFactorInput.get();
 
         // Count scaled elements:
         int n = 0;
@@ -63,15 +73,26 @@ public class ColouredTreeScale extends ColouredTreeOperator {
             return Double.NEGATIVE_INFINITY;
 
         // Scale colour change times:
-        for (Node node : tree.getNodesAsArray())
+        for (Node node : tree.getNodesAsArray()) {
             for (int c = 0; c<cTree.getChangeCount(node); c++) {
                 double oldTime = cTree.getChangeTime(node, c);
                 setChangeTime(node, c, f*oldTime);
                 n += 1;
             }
-
-        // New tree height:
-        double hNew = tree.getRoot().getHeight();
+        }
+        
+        // Scale scalar parameters:
+        for (RealParameter scalar : scalarParamsInput.get()) {
+            double oldValue = scalar.getValue();
+            scalar.setValue(oldValue*f);
+            n += 1;
+        }
+        
+        for (RealParameter scalar : scalarParamsInverseInput.get()) {
+            double oldValue = scalar.getValue();
+            scalar.setValue(oldValue/f);
+            n -= 1;
+        }
 
         // Return Hastings ratio:
         return (n-2)*Math.log(f);
