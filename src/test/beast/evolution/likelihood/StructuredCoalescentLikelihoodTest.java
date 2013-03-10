f66835d009f518486adc63222eadb520b1889c67
Tim Vaughan <tgvaughan@gmail.com>
2013-03-10 20:03:02 +1300

    Updated SC likelihood unit test.

--- src/test/beast/evolution/likelihood/StructuredCoalescentLikelihoodTest.java
+++ src/test/beast/evolution/likelihood/StructuredCoalescentLikelihoodTest.java
@@ -42,12 +42,11 @@ public class StructuredCoalescentLikelihoodTest {
                         "(((A[state=1]:0.25)[state=0]:0.25,B[state=0]:0.5)[state=0]:1.5,"
                         + "(C[state=0]:1.0,D[state=0]:1.0)[state=0]:1.0)[state=0]:0.0;";
 
-		MultiTypeTreeFromNewick ctree = new MultiTypeTreeFromNewick();
-		ctree.initByName(
+		MultiTypeTreeFromNewick mtTree = new MultiTypeTreeFromNewick();
+		mtTree.initByName(
                         "newick", newickStr,
-                        "colourLabel", "state",
-                        "nColours", 2,
-                        "maxBranchColours", 10);
+                        "typeLabel", "state",
+                        "nTypes", 2);
 
 		// Assemble migration model:
 		RealParameter rateMatrix = new RealParameter();
@@ -69,7 +68,7 @@ public class StructuredCoalescentLikelihoodTest {
 		StructuredCoalescentLikelihood likelihood = new StructuredCoalescentLikelihood();
 		likelihood.initByName(
                         "migrationModel", migrationModel,
-                        "colouredTree", ctree);
+                        "multiTypeTree", mtTree);
 
 		double expResult = -16.52831; // Calculated by hand
 		double result = likelihood.calculateLogP();
