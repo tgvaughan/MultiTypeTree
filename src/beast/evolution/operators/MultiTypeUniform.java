0538869af2227a729eeb073b4358654b946dac3f
Tim Vaughan <tgvaughan@gmail.com>
2013-05-21 19:51:52 +1200

    Weird bugs.

--- src/beast/evolution/operators/MultiTypeUniform.java
+++ src/beast/evolution/operators/MultiTypeUniform.java
@@ -18,6 +18,7 @@ package beast.evolution.operators;
 
 
 import beast.core.Description;
+import beast.core.Input;
 import beast.evolution.tree.MultiTypeNode;
 import beast.evolution.tree.Node;
 import beast.util.Randomizer;
@@ -31,6 +32,12 @@ import beast.util.Randomizer;
         + " parent and children.")
 public class MultiTypeUniform extends MultiTypeTreeOperator {
 
+    public Input<Boolean> includeRootInput = new Input<Boolean>("includeRoot",
+            "Allow modification of root node.", false);
+    
+    public Input<Double> rootScaleFactorInput = new Input<Double>("rootScaleFactor",
+            "Root scale factor.", 0.1);
+    
     @Override
     public void initAndValidate() {
     }
@@ -44,33 +51,84 @@ public class MultiTypeUniform extends MultiTypeTreeOperator {
     public double proposal() {
 
         mtTree = multiTypeTreeInput.get();
-
-        // randomly select internal node
-        final int nNodeCount = mtTree.getNodeCount();
-        Node node;
-        do {
-            final int iNodeNr = nNodeCount / 2 + 1 + Randomizer.nextInt(nNodeCount / 2);
-            node = mtTree.getNode(iNodeNr);
-        } while (node.isRoot() || node.isLeaf());
-
-        // upper is first node type change, if there is one, otherwise it's parent height
-        final double fUpper = (((MultiTypeNode)node).getChangeCount() > 0)
-                ? ((MultiTypeNode)node).getChangeTime(0)
-                : node.getParent().getHeight();
         
-        // lower is maximum of childrens' last node type change, if there are any, otherwise of their heights         
-        Node left = node.getLeft();
-        Node right = node.getRight();
-        int lCount = ((MultiTypeNode)left).getChangeCount();
-        int rCount = ((MultiTypeNode)right).getChangeCount();
-        final double fLower = Math.max(
-                (lCount>0 ? (((MultiTypeNode)left).getChangeTime(lCount-1)) : left.getHeight()),
-                (rCount>0 ? (((MultiTypeNode)right).getChangeTime(rCount-1)) : right.getHeight()));
+        int nodeSetSize = mtTree.getInternalNodeCount();
+        if (!includeRootInput.get())
+            nodeSetSize -= 1;
 
-        final double newValue = (Randomizer.nextDouble() * (fUpper - fLower)) + fLower;
-        node.setHeight(newValue);
-
-        return 0.0;
+        // Randomly select event on tree:
+        int event = Randomizer.nextInt(nodeSetSize + mtTree.getTotalNumberOfChanges());
+        
+        
+        MultiTypeNode node = null;
+        int changeIdx = -1;
+        if (event<nodeSetSize)
+            node = (MultiTypeNode)mtTree.getNode(mtTree.getLeafNodeCount() + event);
+        else {
+            event -= nodeSetSize;
+            for (Node thisNode : mtTree.getNodesAsArray()) {
+                if (thisNode.isRoot())
+                    continue;                
+                if (event<((MultiTypeNode)thisNode).getChangeCount()) {
+                    node = (MultiTypeNode)thisNode;
+                    changeIdx = event;
+                    break;
+                }
+                event -= ((MultiTypeNode)thisNode).getChangeCount();
+            }
+        }
+        
+        if (node == null)
+            throw new IllegalStateException("Event selection loop fell through!");
+        
+        if (changeIdx==-1) {
+            if (node.isRoot()) {
+                // Scale distance root and closest event
+                double tmin = Math.max(((MultiTypeNode)node.getLeft()).getFinalChangeTime(),
+                        ((MultiTypeNode)node.getRight()).getFinalChangeTime());
+                
+                double u = Randomizer.nextDouble();
+                double f = u*rootScaleFactorInput.get()
+                        + (1-u)/rootScaleFactorInput.get();
+                
+                double tnew = tmin + f*(node.getHeight()-tmin);
+                
+                node.setHeight(tnew);
+                return -Math.log(f);
+            } else {
+                // Reposition node randomly between closest events
+                double tmin = Math.max(((MultiTypeNode)node.getLeft()).getFinalChangeTime(),
+                        ((MultiTypeNode)node.getLeft()).getFinalChangeTime());
+                
+                double tmax = node.getChangeCount()>0
+                        ? node.getChangeTime(0)
+                        : node.getParent().getHeight();
+                
+                double u = Randomizer.nextDouble();
+                double tnew = u*tmin + (1-u)*tmax;
+                
+                node.setHeight(tnew);
+                return 0.0;
+            }
+        } else {
+            double tmin, tmax;
+            if (changeIdx+1<node.getChangeCount()) 
+                tmax = node.getChangeTime(changeIdx+1);
+            else
+                tmax = node.getParent().getHeight();
+            
+            if (changeIdx-1<0)
+                tmin = node.getHeight();
+            else
+                tmin = node.getChangeTime(changeIdx-1);
+            
+            double u = Randomizer.nextDouble();
+            double tnew = u*tmin + (1-u)*tmax;
+            
+            node.setChangeTime(changeIdx, tnew);
+            return 0.0;
+        }
+        
     }
 
 }
