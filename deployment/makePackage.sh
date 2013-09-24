6827531dc5da521bc1425a03cb6933673fe5b5c3
Tim Vaughan <tgvaughan@gmail.com>
2013-09-25 11:06:26 +1200

    Added shell script for assembling BEAST package.

---
+++ deployment/makePackage.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Assemble BEAST 2 package
+#
+
+PKGNAME=MultiTypeTree
+
+rm -rf $PKGNAME $PKGNAME.zip
+mkdir $PKGNAME
+mkdir $PKGNAME/examples
+mkdir $PKGNAME/lib
+mkdir $PKGNAME/doc
+
+# Create source jar file
+jar cf $PKGNAME/$PKGNAME.src.jar ../src
+
+# Copy over examples
+cp ../examples/simulated_data/full_inference.xml $PKGNAME/examples
+
+# Copy over binaries
+cp ../dist/MultiTypeTree.jar $PKGNAME/lib
+cp ../lib/*.jar $PKGNAME/lib
+
+# Copy over documentation
+
+# Create version.xml
+cat <<EOF > $PKGNAME/version.xml
+<addon name="MultiTypeTree" version="1.0.0">
+    <depends on="beast2" atleast="2.0.2"/>
+</addon>
+
+EOF
+
+# Create archive and clean up
+zip -r $PKGNAME.zip $PKGNAME
+rm -rf $PKGNAME
