6ea861ea7eece83a2cdcce8bcbe546312e6eb71f
Tim Vaughan <tgvaughan@gmail.com>
2013-10-01 11:06:30 +1300

    Added versioning to package creation script.

--- deployment/makePackage.sh
+++ deployment/makePackage.sh
@@ -3,37 +3,44 @@
 # Assemble BEAST 2 package
 #
 
-PKGNAME=MultiTypeTree
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 version"
+    exit
+fi
 
-rm -rf $PKGNAME $PKGNAME.zip
-mkdir $PKGNAME
-mkdir $PKGNAME/examples
-mkdir $PKGNAME/lib
-mkdir $PKGNAME/doc
+PKGNAME=MultiTypeTree
+VERSION=$1
+
+FULLNAME=$PKGNAME-$VERSION
+rm -rf $FULLNAME $FULLNAME.zip
+mkdir $FULLNAME
+mkdir $FULLNAME/examples
+mkdir $FULLNAME/lib
+mkdir $FULLNAME/doc
 
 # Create source jar file
 pushd ..
-jar cf $PKGNAME.src.jar src test
+jar cf $FULLNAME.src.jar src test
 popd
-mv ../$PKGNAME.src.jar $PKGNAME
+mv ../$FULLNAME.src.jar $FULLNAME
 
 # Copy over examples
-cp ../examples/simulated_data/full_inference.xml $PKGNAME/examples
+cp ../examples/simulated_data/full_inference.xml $FULLNAME/examples
 
 # Copy over binaries
-cp ../dist/MultiTypeTree.jar $PKGNAME/lib
-cp ../lib/*.jar $PKGNAME/lib
+cp ../dist/MultiTypeTree.jar $FULLNAME/lib
+cp ../lib/*.jar $FULLNAME/lib
 
 # Copy over documentation
 
 # Create version.xml
-cat <<EOF > $PKGNAME/version.xml
-<addon name="MultiTypeTree" version="1.0.0">
+cat <<EOF > $FULLNAME/version.xml
+<addon name="MultiTypeTree" version="$VERSION">
     <depends on="beast2" atleast="2.0.2"/>
 </addon>
 
 EOF
 
 # Create archive and clean up
-zip -r $PKGNAME.zip $PKGNAME
-rm -rf $PKGNAME
+zip -r $FULLNAME.zip $FULLNAME
+rm -rf $FULLNAME
