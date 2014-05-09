f2d0ee4e99b17779ff37dc77a69ee5b45c8f722f
Tim Vaughan <tgvaughan@gmail.com>
2014-05-09 13:14:04 +1200

    Moved classes to package multitypetree, addressing issue #6.

---
+++ test/multitypetree/coalescent/SCLikelihoodTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2012 Tim Vaughan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package multitypetree.coalescent;
+
+import beast.core.parameter.RealParameter;
+import multitypetree.distributions.StructuredCoalescentTreeDensity;
+import beast.evolution.tree.MigrationModel;
+import beast.evolution.tree.MultiTypeTreeFromNewick;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for StructuredCoalescentLikelihood class methods.
+ *
+ * @author Tim Vaughan
+ */
+public class SCLikelihoodTest {
+
+	/**
+	 * Test of calculateLogP method, of class StructuredCoalescentLikelihood.
+	 */
+	@Test
+	public void testCalculateLogP() throws Exception {
+		System.out.println("SCLikelihoodTest");
+
+		// Assemble test MultiTypeTree:
+		String newickStr =
+                        "(((A[state=1]:0.25)[state=0]:0.25,B[state=0]:0.5)[state=0]:1.5,"
+                        + "(C[state=0]:1.0,D[state=0]:1.0)[state=0]:1.0)[state=0]:0.0;";
+
+		MultiTypeTreeFromNewick mtTree = new MultiTypeTreeFromNewick();
+		mtTree.initByName(
+                        "newick", newickStr,
+                        "typeLabel", "state",
+                        "nTypes", 2);
+
+		// Assemble migration model:
+		RealParameter rateMatrix = new RealParameter();
+		rateMatrix.initByName("value","2.0 1.0");
+		RealParameter popSizes = new RealParameter();
+		popSizes.initByName("value","5.0 10.0");
+		MigrationModel migrationModel = new MigrationModel();
+		migrationModel.initByName(
+                        "rateMatrix", rateMatrix,
+                        "popSizes", popSizes);
+
+		// Set up likelihood instance:
+		StructuredCoalescentTreeDensity likelihood = new StructuredCoalescentTreeDensity();
+		likelihood.initByName(
+                        "migrationModel", migrationModel,
+                        "multiTypeTree", mtTree);
+
+		double expResult = -16.52831; // Calculated by hand
+		double result = likelihood.calculateLogP();
+
+		System.out.println(result);
+		assertEquals(expResult, result, 1e-5);
+	}
+
+}
---
+++ test/multitypetree/operators/TWBR_TS_Test.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2013 Tim Vaughan <tgvaughan@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package multitypetree.operators;
+
+import beast.core.MCMC;
+import beast.core.Operator;
+import beast.core.State;
+import beast.core.parameter.RealParameter;
+import multitypetree.distributions.StructuredCoalescentTreeDensity;
+import beast.evolution.tree.MigrationModel;
+import multitypetree.operators.MultiTypeTreeScale;
+import multitypetree.operators.TypedWilsonBaldingRandom;
+import beast.evolution.tree.MultiTypeTreeFromNewick;
+import beast.util.Randomizer;
+import multitypetree.util.MultiTypeTreeStatLogger;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Tim Vaughan <tgvaughan@gmail.com>
+ */
+public class TWBR_TS_Test {
+ 
+    @Test
+    public void test1() throws Exception {
+        System.out.println("TWBR_test 1");
+        
+        // Fix seed.
+        Randomizer.setSeed(42);
+        
+        // Assemble initial MultiTypeTree
+        String newickStr =
+                "((1[deme='0']:1,2[deme='0']:1)[deme='0']:1,"
+                + "3[deme='0']:2)[deme='0']:0;";
+        
+        MultiTypeTreeFromNewick mtTree = new MultiTypeTreeFromNewick();
+        mtTree.initByName(
+                "newick", newickStr,
+                "typeLabel", "deme",
+                "nTypes", 2);
+        
+        // Assemble migration model:
+        RealParameter rateMatrix = new RealParameter("0.1 0.1");
+        RealParameter popSizes = new RealParameter("7.0 7.0");
+        MigrationModel migModel = new MigrationModel();
+        migModel.initByName(
+                "rateMatrix", rateMatrix,
+                "popSizes", popSizes);
+        
+        // Assemble distribution:
+        StructuredCoalescentTreeDensity distribution =
+                new StructuredCoalescentTreeDensity();
+        distribution.initByName(
+                "migrationModel", migModel,
+                "multiTypeTree", mtTree);
+        
+        // Set up state:
+        State state = new State();
+        state.initByName("stateNode", mtTree);
+        
+        // Set up operator:
+        TypedWilsonBaldingRandom operatorTWBR = new TypedWilsonBaldingRandom();
+        operatorTWBR.initByName(
+                "weight", 1.0,
+                "multiTypeTree", mtTree,
+                "mu", 0.2,
+                "alpha", 0.2);
+        
+        Operator operatorMTTS = new MultiTypeTreeScale();
+        operatorMTTS.initByName(
+                "weight", 1.0,
+                "multiTypeTree", mtTree,
+                "scaleFactor", 0.8,
+                "useOldTreeScaler", false);
+        
+        // Set up stat analysis logger:
+        MultiTypeTreeStatLogger logger = new MultiTypeTreeStatLogger();
+        logger.initByName(
+                "multiTypeTree", mtTree,
+                "burninFrac", 0.2,
+                "logEvery", 1000);
+        
+        // Set up MCMC:
+        MCMC mcmc = new MCMC();
+        mcmc.initByName(
+                "chainLength", "1000000",
+                "state", state,
+                "distribution", distribution,
+                "operator", operatorTWBR,
+                "operator", operatorMTTS,
+                "logger", logger);
+        
+        // Run MCMC:
+        mcmc.run();
+        
+        System.out.format("height mean = %s\n", logger.getHeightMean());
+        System.out.format("height var = %s\n", logger.getHeightVar());
+        System.out.format("height ESS = %s\n", logger.getHeightESS());
+        
+        // Compare analysis results with truth:        
+        boolean withinTol = (logger.getHeightESS()>500)
+                && (Math.abs(logger.getHeightMean()-19)<0.5)
+                && (Math.abs(logger.getHeightVar()-300)<50);
+        
+        Assert.assertTrue(withinTol);
+    }
+    
+    @Test
+    public void test2() throws Exception {
+        System.out.println("TWBR_test 2");
+        
+        // Fix seed.
+        Randomizer.setSeed(42);
+        
+        // Assemble initial MultiTypeTree
+        String newickStr =
+                "((1[deme='1']:1,2[deme='0']:1)[deme='0']:1,"
+                + "3[deme='0']:2)[deme='0']:0;";
+        
+        MultiTypeTreeFromNewick mtTree = new MultiTypeTreeFromNewick();
+        mtTree.initByName(
+                "newick", newickStr,
+                "typeLabel", "deme",
+                "nTypes", 2);
+        
+        // Assemble migration model:
+        RealParameter rateMatrix = new RealParameter("0.1 0.1");
+        RealParameter popSizes = new RealParameter("7.0 7.0");
+        MigrationModel migModel = new MigrationModel();
+        migModel.initByName(
+                "rateMatrix", rateMatrix,
+                "popSizes", popSizes);
+        
+        // Assemble distribution:
+        StructuredCoalescentTreeDensity distribution =
+                new StructuredCoalescentTreeDensity();
+        distribution.initByName(
+                "migrationModel", migModel,
+                "multiTypeTree", mtTree);
+        
+        // Set up state:
+        State state = new State();
+        state.initByName("stateNode", mtTree);
+        
+        // Set up operator:
+        TypedWilsonBaldingRandom operatorTWBR = new TypedWilsonBaldingRandom();
+        operatorTWBR.initByName(
+                "weight", 1.0,
+                "multiTypeTree", mtTree,
+                "mu", 0.2,
+                "alpha", 0.2);
+        
+        Operator operatorMTTS = new MultiTypeTreeScale();
+        operatorMTTS.initByName(
+                "weight", 1.0,
+                "multiTypeTree", mtTree,
+                "scaleFactor", 0.8,
+                "useOldTreeScaler", false);
+        
+        // Set up stat analysis logger:
+        MultiTypeTreeStatLogger logger = new MultiTypeTreeStatLogger();
+        logger.initByName(
+                "multiTypeTree", mtTree,
+                "burninFrac", 0.1,
+                "logEvery", 1000);
+        
+        // Set up MCMC:
+        MCMC mcmc = new MCMC();
+        mcmc.initByName(
+                "chainLength", "1000000",
+                "state", state,
+                "distribution", distribution,
+                "operator", operatorTWBR,
+                "operator", operatorMTTS,
+                "logger", logger);
+        
+        // Run MCMC:
+        mcmc.run();
+        
+        System.out.format("height mean = %s\n", logger.getHeightMean());
+        System.out.format("height var = %s\n", logger.getHeightVar());
+        System.out.format("height ESS = %s\n", logger.getHeightESS());
+        
+        // Compare analysis results with truth:        
+        boolean withinTol = (logger.getHeightESS()>200)
+                && (Math.abs(logger.getHeightMean()-23)<1)
+                && (Math.abs(logger.getHeightVar()-300)<30);
+        
+        Assert.assertTrue(withinTol);
+    }
+}
