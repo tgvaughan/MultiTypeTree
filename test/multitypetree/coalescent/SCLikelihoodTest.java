b924513fa6669f5d002b3a8eef172fabfa640e2a
Tim Vaughan <timv@ughan.xyz>
2022-11-07 15:45:38 +0100

    Fixed unit tests and build.xml.

--- test/multitypetree/coalescent/SCLikelihoodTest.java
+++ test/multitypetree/coalescent/SCLikelihoodTest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package test.multitypetree;
+package multitypetree.coalescent;
 
 import org.junit.Test;
 
